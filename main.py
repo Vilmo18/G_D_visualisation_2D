4fcb8f3f4948ac2bc79a5085a77343464f8b4c7d
Vilmo18 <kvilmorin@gmail.com>
2023-09-28 16:31:24 +0100

    Ajustement

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Créez une figure et un axe
+# Créez une figure et un /
 fig, ax = plt.subplots()
 
 # Créez une ligne représentant la courbe
@@ -13,15 +13,14 @@ line, = ax.plot(x, y)
 # Créez le point que vous souhaitez animer
 point, = ax.plot([], [], 'ro')
 
-# Liste de nouvelles positions du point
 
-
+#Algorithme de la descente de gradient
 def gradient(x0, l_rate=1, iter=1000):
     def f(z): return 2*z
     x1 = x0
     abs = []
     ord = []
-    for i in range(10):
+    for i in range(iter):
         x1 = x1-l_rate*f(x1)
         y1 = x1**2
         abs.append(x1)
@@ -30,25 +29,21 @@ def gradient(x0, l_rate=1, iter=1000):
     return abs, ord
 
 
-abs, ord = gradient(-3, 0.1, 100)
+# Liste de nouvelles positions du point
+abs, ord = gradient(-3, 1, 100)
 new_positions = [(x_val, y_val) for x_val, y_val in zip(abs, ord)]
 
 
 # Fonction d'initialisation de l'animation
-
-
 def init():
     point.set_data([], [])
     return point,
 
 # Fonction d'animation pour mettre à jour la position du point
-
-
 def animate(frame):
     x_val, y_val = new_positions[frame]
     point.set_data(x_val, y_val)
     return point,
-
 
 # Créez l'animation
 ani = FuncAnimation(fig, animate, frames=len(
